db07947da9cd6413b6a59321d50c07f82be8a091
DungKM <hoanganhdung13122000@gmail.com>
2025-05-06 09:00:31 +0700

    feat👻: add user model and actions; define user roles and statuses in enums

---
+++ src/app/database/user.model.ts
@@ -0,0 +1,28 @@
+import { EUserRole, EUserStatus } from "@/components/types/enums";
+import { Document, model, models, Schema } from "mongoose"
+import { use } from "react";
+
+export interface IUser extends Document {
+    clerkId: string;
+    name: string;
+    username: string;
+    email_address: string;
+    avatar: string;
+    status: EUserStatus;
+    courses: Schema.Types.ObjectId[];
+    role: EUserRole;
+    createdAt: Date;
+}
+const userSchema = new Schema<IUser>({
+    clerkId: { type: String },
+    name: { type: String },
+    username: { type: String },
+    email_address: { type: String },
+    avatar: { type: String },
+    courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
+    createdAt: { type: Date, default: Date.now },
+    role: { type: String, enum: Object.values(EUserRole), default: EUserRole.USER },
+    status: { type: String, enum: Object.values(EUserStatus), default: EUserStatus.ACTIVE },
+}, { timestamps: true });
+const User = models.User || model<IUser>("User", userSchema);
+export default User;
---
+++ src/lib/actions/user.actions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import User, { IUser } from "@/app/database/user.model";
+import { connectToDatabase } from "../mongoose";
+
+export default async function createUser(params: IUser){
+    try {
+       connectToDatabase();
+       const newUser = await User.create(params);
+       return newUser;
+    }
+    catch (error) {
+        console.log(error);
+        return null;
+    }
+}
