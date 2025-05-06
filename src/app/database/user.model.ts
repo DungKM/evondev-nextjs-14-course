988e2d21e9b85f58a05cbddbe849a2702eb9f65f
DungKM <hoanganhdung13122000@gmail.com>
2025-05-06 23:43:06 +0700

    feat: implement webhook handler for user events; update user model to standardize email and created_at fields

--- src/app/database/user.model.ts
+++ src/app/database/user.model.ts
@@ -6,21 +6,21 @@ export interface IUser extends Document {
     clerkId: string;
     name: string;
     username: string;
-    email_address: string;
+    email: string;
     avatar: string;
     status: EUserStatus;
     courses: Schema.Types.ObjectId[];
     role: EUserRole;
-    createdAt: Date;
+    created_at: Date;
 }
 const userSchema = new Schema<IUser>({
     clerkId: { type: String },
     name: { type: String },
     username: { type: String, unique: true, required: true },
-    email_address: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
     avatar: { type: String },
     courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
-    createdAt: { type: Date, default: Date.now },
+    created_at: { type: Date, default: Date.now },
     role: { type: String, enum: Object.values(EUserRole), default: EUserRole.USER },
     status: { type: String, enum: Object.values(EUserStatus), default: EUserStatus.ACTIVE },
 }, { timestamps: true });
